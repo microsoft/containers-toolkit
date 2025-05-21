2d045cbf0144ae9a4657415f3ea2377e1eb22244
Tina Murimi <mor.devx@outlook.com>
2025-05-21 16:49:07 +0300

    Import logger modules

--- containers-toolkit/Public/ContainerdTools.psm1
+++ containers-toolkit/Public/ContainerdTools.psm1
@@ -7,6 +7,7 @@
 ###########################################################################
 
 using module "..\Private\CommonToolUtilities.psm1"
+using module "..\Private\logger.psm1"
 
 $ModuleParentPath = Split-Path -Parent $PSScriptRoot
 Import-Module -Name "$ModuleParentPath\Private\CommonToolUtilities.psm1" -Force
